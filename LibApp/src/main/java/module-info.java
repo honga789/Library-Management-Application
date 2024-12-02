4ed3ea7031715f705cb4380fab5490c420f388a6
dauduchieu <dauduchieu.ddh775@gmail.com>
2024-12-02 19:10:08 +0700

    add module-info.java to open to javafx

--- LibApp/src/main/java/module-info.java
+++ LibApp/src/main/java/module-info.java
@@ -2,6 +2,11 @@ module dha.libapp {
     requires javafx.controls;
     requires javafx.fxml;
 
+    exports dha.libapp.controllers;
+    opens dha.libapp.controllers to javafx.fxml;
+
+    opens dha.libapp.views to javafx.fxml;
+
     opens dha.libapp to javafx.fxml;
     exports dha.libapp;
 }
