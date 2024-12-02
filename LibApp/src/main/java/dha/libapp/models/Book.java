68dc8f46b37d733ed1638f9c91d1d1da5979c1c9
honga789 <honganhnguyenpro655@gmail.com>
2024-12-03 00:32:48 +0700

    Create Book.java and GenreType.jva

--- LibApp/src/main/java/dha/libapp/models/Book.java
+++ LibApp/src/main/java/dha/libapp/models/Book.java
@@ -1,4 +1,156 @@
 package dha.libapp.models;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Book {
+    private int bookId;
+    private String ISBN;
+    private String title;
+    private String author;
+    private String publisher;
+    private Date publicationDate;
+    private int quantity;
+    private String description;
+    private String coverImagePath;
+    private ArrayList<GenreType> genreList;
+
+    public Book() {
+    }
+
+    public Book(int bookId, String ISBN, String title, String author, String publisher,
+                Date publicationDate, int quantity, String description, String coverImagePath,
+                ArrayList<GenreType> genreList)
+    {
+        this.bookId = bookId;
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.publicationDate = publicationDate;
+        this.quantity = quantity;
+        this.description = description;
+        this.coverImagePath = coverImagePath;
+        this.genreList = genreList;
+    }
+
+    public Book(Book book) {
+        this.bookId = book.getBookId();
+        this.ISBN = book.getISBN();
+        this.title = book.getTitle();
+        this.author = book.getAuthor();
+        this.publisher = book.getPublisher();
+        this.publicationDate = book.getPublicationDate();
+        this.quantity = book.getQuantity();
+        this.description = book.getDescription();
+        this.coverImagePath = book.getCoverImagePath();
+        this.genreList = book.getGenreList();
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public Book setBookId(int bookId) {
+        this.bookId = bookId;
+        return this;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public Book setISBN(String ISBN) {
+        this.ISBN = ISBN;
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Book setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Book setAuthor(String author) {
+        this.author = author;
+        return this;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public Book setPublisher(String publisher) {
+        this.publisher = publisher;
+        return this;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public Book setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+        return this;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Book setQuantity(int quantity) {
+        this.quantity = quantity;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Book setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getCoverImagePath() {
+        return coverImagePath;
+    }
+
+    public Book setCoverImagePath(String coverImagePath) {
+        this.coverImagePath = coverImagePath;
+        return this;
+    }
+
+    public ArrayList<GenreType> getGenreList() {
+        return genreList;
+    }
+
+    public Book setGenreList(ArrayList<GenreType> genreList) {
+        this.genreList = genreList;
+        return this;
+    }
+
+    public String toString() {
+        return "bookId: " + bookId + "\n"
+                + "ISBN: " + ISBN + "\n"
+                + "Title: " + title + "\n"
+                + "Author: " + author + "\n"
+                + "Publisher: " + publisher + "\n"
+                + "Publication Date: " + publicationDate + "\n"
+                + "Quantity: " + quantity + "\n"
+                + "Description: " + description + "\n"
+                + "Cover Image: " + coverImagePath + "\n"
+                + "GenreList: " + genreList;
+    }
+
+    public void displayInfo() {
+        System.out.println(this.toString());
+    }
 }
---
+++ LibApp/src/main/java/dha/libapp/models/GenreType.java
@@ -0,0 +1,59 @@
+package dha.libapp.models;
+
+public class GenreType {
+    private int genreId;
+    private String genreName;
+    private float weight;
+
+    GenreType() {
+    }
+
+    GenreType(int genreId, String genreName, float weight) {
+        this.genreId = genreId;
+        this.genreName = genreName;
+        this.weight = weight;
+    }
+
+    GenreType(GenreType genreType) {
+        this.genreId = genreType.getGenreId();
+        this.genreName = genreType.getGenreName();
+        this.weight = genreType.getWeight();
+    }
+
+    public int getGenreId() {
+        return genreId;
+    }
+
+    public GenreType setGenreId(int genreId) {
+        this.genreId = genreId;
+        return this;
+    }
+
+    public String getGenreName() {
+        return genreName;
+    }
+
+    public GenreType setGenreName(String genreName) {
+        this.genreName = genreName;
+        return this;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public GenreType setWeight(float weight) {
+        this.weight = weight;
+        return this;
+    }
+
+    public String toString() {
+        return "genreId: " + genreId + "\n"
+                + "genreName: " + genreName + "\n"
+                + "weight: " + weight;
+    }
+
+    public void displayInfo() {
+        System.out.println(this.toString());
+    }
+}
