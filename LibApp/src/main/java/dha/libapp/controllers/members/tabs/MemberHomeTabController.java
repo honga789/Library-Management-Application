d7d4cb0b2f5305c2f3db2b2b40166a8766d77e8f
dauduchieu <dauduchieu.ddh775@gmail.com>
2024-12-04 22:11:25 +0700

    call image api to show image at index

--- LibApp/src/main/java/dha/libapp/controllers/members/tabs/MemberHomeTabController.java
+++ LibApp/src/main/java/dha/libapp/controllers/members/tabs/MemberHomeTabController.java
@@ -4,12 +4,18 @@ import dha.libapp.dao.BookDAO;
 import dha.libapp.models.Book;
 import dha.libapp.services.SessionService;
 import dha.libapp.services.members.tabs.MemberHomeTabService;
+import dha.libapp.utils.API.ExecutorHandle;
+import dha.libapp.utils.API.Image.ImageAPI;
+import dha.libapp.utils.API.Image.ImageFetchCallback;
+import dha.libapp.utils.API.Image.ImageTask;
 import dha.libapp.utils.ListView.BookListView;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 import java.net.URL;
@@ -45,6 +51,9 @@ public class MemberHomeTabController implements Initializable {
     @FXML
     private Label bookDetailDescription;
 
+    @FXML
+    private ImageView bookDetailImage;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         instance = this;
@@ -69,6 +78,21 @@ public class MemberHomeTabController implements Initializable {
         bookDetailName.setText(book.getTitle());
         bookDetailAuthor.setText(book.getAuthor());
         bookDetailDescription.setText(book.getDescription());
+
+        ImageTask imageTask = ImageAPI.getImageWithUrl(book.getCoverImagePath(), new ImageFetchCallback() {
+            @Override
+            public void onSuccess(Image image) {
+                bookDetailImage.setImage(image);
+            }
+
+            @Override
+            public void onFailure(Exception ex) {
+
+            }
+        });
+
+        ExecutorHandle.getInstance().addTask(imageTask);
+
     }
 
     public void renderRecommendationBooks(List<Book> bookList) {
