30ee860589f6fd89d255c75f92572ef4f864ee8e
dauduchieu <dauduchieu.ddh775@gmail.com>
2024-12-11 09:23:47 +0700

    add javadoc comment for DAOSync/utils

--- LibApp/src/main/java/dha/libapp/syncdao/utils/DAOExecuteCallback.java
+++ LibApp/src/main/java/dha/libapp/syncdao/utils/DAOExecuteCallback.java
@@ -1,6 +1,23 @@
 package dha.libapp.syncdao.utils;
 
+/**
+ * Callback interface for execution operations with a return type.
+ * Provides methods to handle success and error scenarios during DAO executions.
+ *
+ * @param <T> the type of result expected from the execution.
+ */
 public interface DAOExecuteCallback<T> {
+    /**
+     * Called when the execution operation is successful.
+     *
+     * @param result the result of the successful operation.
+     */
     void onSuccess(T result);
+
+    /**
+     * Called when an error occurs during the execution operation.
+     *
+     * @param e the exception encountered during the operation.
+     */
     void onError(Throwable e);
 }
